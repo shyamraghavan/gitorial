f0977a74268c7c2d9d9072885c5b15b472b10f22
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-02-01 02:03:54 -0500

    Does more cool stuf

---
+++ pastebin.js
@@ -0,0 +1,44 @@
+{
+    "provider": "github",
+    "id": 5544532,
+    "displayName": "Jacob Zimmerman",
+    "username": "Z1MM32M4N",
+    "profileUrl": "https://github.com/Z1MM32M4N",
+    "emails": [
+        {
+            "value": "jzimmerman@cmu.edu"
+        }
+    ],
+    "_json": {
+        "login": "Z1MM32M4N",
+        "id": 5544532,
+        "avatar_url": "https://gravatar.com/avatar/258d0924cabf7f1a13eab9daf854e9fa?d=https%3A%2F%2Fidenticons.github.com%2Ff24d554fd4d19babc408548ed80597d8.png&r=x",
+        "gravatar_id": "258d0924cabf7f1a13eab9daf854e9fa",
+        "url": "https://api.github.com/users/Z1MM32M4N",
+        "html_url": "https://github.com/Z1MM32M4N",
+        "followers_url": "https://api.github.com/users/Z1MM32M4N/followers",
+        "following_url": "https://api.github.com/users/Z1MM32M4N/following{/other_user}",
+        "gists_url": "https://api.github.com/users/Z1MM32M4N/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/Z1MM32M4N/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/Z1MM32M4N/subscriptions",
+        "organizations_url": "https://api.github.com/users/Z1MM32M4N/orgs",
+        "repos_url": "https://api.github.com/users/Z1MM32M4N/repos",
+        "events_url": "https://api.github.com/users/Z1MM32M4N/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/Z1MM32M4N/received_events",
+        "type": "User",
+        "site_admin": false,
+        "name": "Jacob Zimmerman",
+        "company": null,
+        "blog": "www.jacobzimmerman.me",
+        "location": "Pittsburgh",
+        "email": "jzimmerman@cmu.edu",
+        "hireable": true,
+        "bio": null,
+        "public_repos": 9,
+        "public_gists": 0,
+        "followers": 5,
+        "following": 2,
+        "created_at": "2013-09-25T20:40:35Z",
+        "updated_at": "2014-02-01T02:34:26Z"
+    }
+}
