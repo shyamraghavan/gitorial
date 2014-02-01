de347d2ffe11c1851b69773055eb9b02ce21875c
Aaron Gutierrez <amgutier@andrew.cmu.edu>
2014-02-01 05:21:38 -0500

    aws file stuf work swag

--- routes/view.js
+++ routes/view.js
@@ -1,5 +1,17 @@
 module.exports = function(db) {
   return function (req, res) {
-    res.render("view", {curStep: req.param("curStep")});
+    var AWS = require("aws-sdk");
+    var s3 = new AWS.S3();
+    var fs = require('fs');
+    var params = {Bucket: 'gitorial', Key: req.param("id")};
+    var file= fs.createWriteStream('./temp.tmp');
+    try {
+      s3.getObject(params, function(error, data) {
+        res.render("view", {curStep: req.param("curStep"), data: data.Body.toString()});
+      });
+    } catch (err) {
+      console.log(err);
+      return;
+    }
   }
 };
