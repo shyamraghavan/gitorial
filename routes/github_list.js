23c43a2d1b52910285d7aae13611992e69fb23e6
Shyam Raghavan <shyam.raghavan.1@gmail.com>
2014-02-01 08:38:10 -0500

    working github

--- routes/github_list.js
+++ routes/github_list.js
@@ -1,11 +1,38 @@
 function separate(json)
 {
-  console.log(json[1]);
+  results = [];
+
+  for (i = 0; i < json.length; i++)
+  {
+    results.push(json[i]["sha"]);
+  }
+
+  console.log(results);
+  return results;
 }
 
-function log_screen(json)
+function log_screen(json, username, repo_name)
 {
+  var GitHubApi = require('github');
+
+  var github = new GitHubApi({
+      version: "3.0.0",
+      timeout: 5000
+  });
+
   list_sha = separate(json);
+
+  for (i = 0; i < list_sha.length - 1; i++)
+  {
+    github.repos.compareCommits({
+      user: username, 
+      repo: repo_name,
+      base: list_sha[0],
+      head: list_sha[1]
+    }, function(err, res) {
+      console.log(res["diff_url"].toString());
+    });
+  }
 }
 
 function getCommitsList(username, repo_name){
@@ -23,8 +50,8 @@ function getCommitsList(username, repo_name){
       user: username,
       repo: repo_name
   }, function(err, res) {
-        commitsList = JSON.stringify(res);
-        log_screen(commitsList);
+      commitsList = res;
+      log_screen(commitsList, username, repo_name);
   });
 }
 
