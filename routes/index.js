a4fef463a9b336200ab123fd7b0a4ca7bc345d5f
Aaron Gutierrez <amgutier@andrew.cmu.edu>
2014-02-01 09:12:24 -0500

    updated index page once logged in

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 module.exports = function(db) {
   return function (req, res) {
-    res.render('index', {user: req.user});
+    var AWS = require("aws-sdk");
+    var s3 = new AWS.S3();
+
+    if (req.user) {
+      var params = {Bucket: "gitorial", Key: req.user.username};
+      try {
+        s3.getObject(params, function(error, data) {
+          if (data == null) 
+            res.render('index', {user: req.user});
+          else {
+            console.log(data.Body.toString());
+            res.render('index', {user: req.user, tutorials: JSON.parse(data.Body.toString())});
+          }
+        });
+      } catch (err) {
+        res.render('index', {user: req.user});
+      }
+    } else {
+      res.render('index');
+    }
   }
 };
