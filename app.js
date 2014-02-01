497823f9abffee570999a08d03f9577ba1b60535
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-02-01 10:11:04 -0500

    forgot a line

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-<<<<<<< Updated upstream
 var GitHubApi = require("github");
 
 //===============================================
